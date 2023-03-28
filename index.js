b0e947f7fe54c1658e94546d22a6dd096052790d
edu420092@gmail.com <carlos.esilva@flexcontact.com.br>
2023-03-28 12:16:10 -0300

    body-parser

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const route = 10;
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
+
+//========= bodyParser =========//
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 //========= Rotas =========//
 app.get("/",(req,res) => {
@@ -13,6 +18,15 @@ app.get("/",(req,res) => {
 app.get("/perguntar",(req,res) => {
     res.render("toAsk");
 });
+
+
+app.post("/salvarPergunta",(req,res) => {
+    let titulo = req.body.titulo;
+    let desc = req.body.desc;
+
+    res.send("<h3>Formulario Recebido</h3><p><b>Titulo: </b>" + titulo +"</p><p><b>Descrição: </b>" + desc +"</p>");
+});
+
 
 //========= Servidor =========//
 app.listen(route,(error) => {
